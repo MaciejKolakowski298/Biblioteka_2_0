d51c5f9d49d04a4f1f490ccbac154be2c3dc4ed3
Maciej Kolakowski <maciej.kolakowski@gmail.com>
2023-02-19 14:03:10 +0100

    Add wypożyczenia i zwroty - still not final ver.

--- Website/views.py
+++ Website/views.py
@@ -38,3 +38,66 @@ def add():
                 books_list.append(BookSchema(book))
             return render_template('home.html', books=books_list, books_no=len(books))
     return render_template('add.html')
+
+@views.route('/edit', methods=['GET', 'POST'])
+
+def edit():
+    if request.method=='POST':
+        new_title=request.form.get('title')
+        new_available=request.form.get('available')
+        book=Book.query.get(new_title)
+        if book:
+            book=Book.query.get(new_title)
+            book.available=new_available
+            db.session.commit()
+            flash(f'book updated ({new_title})')
+        return home()
+    else:
+        book=None
+        if request.args:
+            book=BookSchema(Book.query.get(request.args['title']))
+        return render_template('edit.html', book=book)
+    
+@views.route('/lend', methods=['GET', 'POST'])
+
+def lend():
+    if request.method=='POST':
+        new_title=request.form.get('title')
+        new_available=request.form.get('available')
+        new_user=request.form.get('user')
+        book=Book.query.get(new_title)
+        if book:
+            book=Book.query.get(new_title)
+            user=User(name=new_user, book=new_title)
+            if new_user not in [user.name for user in User.query.all()]:
+                book.available=new_available
+                db.session.add(user)
+                db.session.commit()
+                flash(f'book lent ({new_title} -> {new_user})')
+            else:
+                flash('user already has another book')
+        return home()
+    else:
+        book=None
+        if request.args:
+            book=BookSchema(Book.query.get(request.args['title']))
+        return render_template('lend.html', book=book)
+    
+@views.route('/return', methods=['GET', 'POST'])
+
+def return_book():
+    new_title=request.args['title']
+    new_user=request.args['user']
+    book=Book.query.get(new_title)
+    if book:
+        book=Book.query.get(new_title)
+        book.available='on'
+        user=User.query.get(new_user)
+        db.session.delete(user)
+        db.session.commit()
+        flash('book returned')
+    else:
+        flash('something wrong!')
+    return redirect(url_for('views.home'))
+
+
